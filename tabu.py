57f63d378c7679d4d608c55bf7bec41ce21c0c22
Akshay Budhkar <abudhkar@uwaterloo.ca>
2016-06-27 12:42:09 -0400

    Adds the cost function for a given solution instance

--- tabu.py
+++ tabu.py
@@ -5,6 +5,33 @@ Created on Mon Jun 27 10:59:15 2016
 @author: akshaybudhkar
 """
 import csv
+import random
+
+def get_column(index, matrix):
+    return [row[index] for row in matrix]
+
+def calculate_cost(instance, flow_arr, distance_arr):
+    permutation_flow = [[0 for i in range(20)] for j in range(20)]
+    cost = 0
+        
+    for i in range(len(instance)):
+        permutation_flow[i] = flow_arr[instance[i]]
+    
+    permut_flow_permut = [[0 for i in range(20)] for j in range(20)]
+    
+    for i in range(len(instance)):
+        flow_col = get_column(instance[i], permutation_flow)
+        
+        for j in range(len(permut_flow_permut)):
+            permut_flow_permut[j][instance[i]] = flow_col[j]
+    
+    for i in range(len(distance_arr)):
+        for j in range(len(distance_arr[0])):
+            cost += permut_flow_permut[i][j] + distance_arr[i][j]
+            
+    return cost
+    
+    
 
 # Initial flow and distance arrays setup
 flow_file = open('flow.csv', 'r')
@@ -28,4 +55,10 @@ Defined as departments as rows and locations as columns (a 2D Dimensional Array)
 """
 tabu_matrix = [[0 for i in range(20)] for j in range(20)]
 
-print(tabu_matrix)
+solution_instance = [i for i in range(20)]
+
+# Shuffle for random initial solution
+random.shuffle(solution_instance)
+
+print(calculate_cost(solution_instance, flow_arr, distance_arr))
+print(solution_instance)
