eefa3b15d7ec5f2a9d8e06ea854b3f721ab60dfb
Akshay Budhkar <abudhkar@uwaterloo.ca>
2016-06-27 14:18:31 -0400

    Implements func to calculate most optimal neighbor - something probably wrong with the cost function, always returns 2276

--- tabu.py
+++ tabu.py
@@ -6,7 +6,7 @@ Created on Mon Jun 27 10:59:15 2016
 """
 import csv
 import random
-
+import math
 def get_column(index, matrix):
     return [row[index] for row in matrix]
 
@@ -32,7 +32,22 @@ def calculate_cost(instance, flow_arr, distance_arr):
     return cost
     
     
-
+def get_most_optimal_neighbor(instance, flow_arr, distance_arr):
+    best_cost = float("inf")
+    best_instance = None
+    
+    for i in range(len(instance)):
+        for j in range(len(instance)):
+            if i != j:
+                new_instance = instance[:]
+                new_instance[i], new_instance[j] = new_instance[j], new_instance[i]
+                new_cost = calculate_cost(new_instance, flow_arr, distance_arr)
+                if new_cost < best_cost:
+                    best_cost = new_cost
+                    best_instance = new_instance
+    
+    return best_instance
+            
 # Initial flow and distance arrays setup
 flow_file = open('flow.csv', 'r')
 distance_file = open('distance.csv', 'r')
@@ -55,10 +70,11 @@ Defined as departments as rows and locations as columns (a 2D Dimensional Array)
 """
 tabu_matrix = [[0 for i in range(20)] for j in range(20)]
 
-solution_instance = [i for i in range(20)]
+init_soln = [i for i in range(20)]
 
 # Shuffle for random initial solution
-random.shuffle(solution_instance)
+random.shuffle(init_soln)
 
-print(calculate_cost(solution_instance, flow_arr, distance_arr))
-print(solution_instance)
+print(init_soln)
+print(calculate_cost(init_soln, flow_arr, distance_arr))
+print(get_most_optimal_neighbor(init_soln, flow_arr, distance_arr))
