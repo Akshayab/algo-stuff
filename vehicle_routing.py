939fba3fc839afd601c4937e74294b0484a4220d
Akshay Budhkar <abudhkar@uwaterloo.ca>
2016-06-28 13:01:52 -0400

    Defines the neighborhood function

--- vehicle_routing.py
+++ vehicle_routing.py
@@ -19,6 +19,7 @@ def get_random_solution(lst, min_split, no_of_splits):
         size -= s
     yield list(itr)
 
+
 def calculate_cost(solution, depot, distance):
     cost = 0    
     for i in range(len(solution)):
@@ -34,7 +35,36 @@ def calculate_cost(solution, depot, distance):
                 cost += distance[current][nxt]
                 
     return cost
-                
+    
+"""
+Neighborhood Definition Rules:
+- You can move a city from one route to another
+-- When a city is added to a new route, it is always added at the end
+-- Do not move the city if it is the only one serviced by the vehicle
+- You cannot switch cities in the same route
+-- It doesn't provide as much diversification
+"""
+
+def get_random_neighbor(solution): 
+    move_from = random.randint(0, len(solution) - 1)
+    
+    # Ensure we move from a city with more than one city
+    while len(solution[move_from]) <= 1:
+        move_from = random.randint(0, len(solution) - 1)
+        
+    move_to = random.randint(0, len(solution) - 1)
+    
+    # Ensure we move to a different route
+    while move_to == move_from:
+        move_to = random.randint(0, len(solution) - 1)
+        
+    city_to_move = solution[move_from][random.randint(0, len(solution[move_from]) - 1)]
+    solution[move_from].remove(city_to_move)
+    
+    solution[move_to].append(city_to_move)
+    
+    return solution
+
 # Note: vehicles are cities are zero indexed
 vehicles = 3
 cities = 9
@@ -59,3 +89,8 @@ init_soln = list(get_random_solution(range(cities), 1, vehicles))
 
 print(init_soln)
 print(calculate_cost(init_soln, Depot, D))
+
+nbr = get_random_neighbor(init_soln)
+
+print(nbr)
+print(calculate_cost(nbr, Depot, D))
