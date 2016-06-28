dc8b66415914e9e2a8a2ccbc77810c55e57829fc
Akshay Budhkar <abudhkar@uwaterloo.ca>
2016-06-28 12:42:03 -0400

    Defines the cost function

--- vehicle_routing.py
+++ vehicle_routing.py
@@ -19,6 +19,22 @@ def get_random_solution(lst, min_split, no_of_splits):
         size -= s
     yield list(itr)
 
+def calculate_cost(solution, depot, distance):
+    cost = 0    
+    for i in range(len(solution)):
+        for j in range(len(solution[i])):
+            current = solution[i][j]
+            # Cost from the depot
+            if j == 0 or j == (len(solution[i]) - 1):
+                cost += depot[current]
+            
+            # Cost between cities
+            if j != (len(solution[i]) - 1):
+                nxt = solution[i][j+1]
+                cost += distance[current][nxt]
+                
+    return cost
+                
 # Note: vehicles are cities are zero indexed
 vehicles = 3
 cities = 9
@@ -39,6 +55,7 @@ Solution Definition:
 Consider a solution to be a 2D Matrix with row n representing the cities
 serviced by vehicle n. Every row will have the cities processed in order
 """
-init_soln = get_random_solution(range(cities), 1, vehicles)
+init_soln = list(get_random_solution(range(cities), 1, vehicles))
 
-print(list(init_soln))
+print(init_soln)
+print(calculate_cost(init_soln, Depot, D))
