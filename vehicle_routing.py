a085d30b8bee50a342d7bb4827d1eb3278bfb176
Akshay Budhkar <abudhkar@uwaterloo.ca>
2016-06-28 13:23:56 -0400

    Implements Simulated Annealing to find a decent solution for the VPR

--- vehicle_routing.py
+++ vehicle_routing.py
@@ -4,8 +4,11 @@ Created on Tue Jun 28 11:58:24 2016
 
 @author: akshaybudhkar
 Solution to Question 4 of Assignment 2
+Best result so far for 9 cities, 3 vehicles:
+([[1, 8, 3], [7], [5, 4, 2, 0, 6]], [[1, 8, 3], [7], [5, 4, 2, 0, 6]], 163)
 """
 import random
+import math
 from itertools import islice
 
 def get_random_solution(lst, min_split, no_of_splits):
@@ -64,8 +67,54 @@ def get_random_neighbor(solution):
     solution[move_to].append(city_to_move)
     
     return solution
-
-# Note: vehicles are cities are zero indexed
+    
+def simulated_annealing(solution, depot, distance):
+    current = solution
+    current_cost = calculate_cost(current, depot, distance)
+    best = solution
+    best_cost = current_cost
+    
+    current_temp = 500
+    final_temp = 1
+    alpha = 0.85
+    max_iterations = 100
+    
+    flop_iterations = 0
+    
+    while current_temp > final_temp and flop_iterations < 5000:
+        iterations = 0
+        
+        while iterations < max_iterations:
+            nbr = get_random_neighbor(current)
+            nbr_cost = calculate_cost(nbr, depot, distance)
+            change = nbr_cost - current_cost
+            
+            # If we improve, make that change
+            if change < 0:
+                current = nbr
+                current_cost = nbr_cost
+            else:
+                x = random.random()
+                if x < math.exp(-1*change/current_temp):
+                    current = nbr
+                    current_cost = nbr_cost
+            
+            iterations += 1
+        
+        final_temp = final_temp*alpha
+        
+        if current_cost < best_cost:
+            flop_iterations = 0
+            best_cost = current_cost
+            best = current
+        else:
+            flop_iterations += 1
+            
+        print(current_cost)
+    
+    return (current, best, best_cost)
+        
+# Note: vehicles and cities are zero indexed
 vehicles = 3
 cities = 9
 
@@ -94,3 +143,5 @@ nbr = get_random_neighbor(init_soln)
 
 print(nbr)
 print(calculate_cost(nbr, Depot, D))
+
+print(simulated_annealing(init_soln, Depot, D))
