c94b86455dc353c55667aac7e56e4a6ee2351ef6
Akshay Budhkar <abudhkar@uwaterloo.ca>
2016-07-26 12:42:29 -0400

    Implements GA complete

--- pid.py
+++ pid.py
@@ -6,6 +6,7 @@ Created on Mon Jul 25 13:16:46 2016
 """
 import control
 import random
+import matplotlib.pyplot as plt
 
 # Evalutate fiteness of a solution
 def fitnessFunction(Kp, Ti, Td):
@@ -42,7 +43,7 @@ def fitnessFunction(Kp, Ti, Td):
         if abs(yout[-i]/yout[-1]) > 1.02:
             Ts = t[len(yout) - i] - t[0]
     
-    return 1/(OS + Tr + Ts + error*error)*1000
+    return 1/(OS + Tr + Ts + error*error*10)*100000
     
 # Generate initial population set
 def generate_initial_set(size):
@@ -65,20 +66,94 @@ def generate_initial_set(size):
      
     return refined_set
 
-    
-def crossover(list):
-    return list
-    
 
-def mutate(list):
-    return list
+def check_limits(K, Ti, Td):
+    if K < 2:
+        K = 2
+    
+    if K > 18:
+        K = 18
+    
+    if Ti < 1.05:
+        Ti = 1.05
+        
+    if Ti > 9.42:
+        Ti = 9.42
+        
+    if Td < 0.26:
+        Td = 0.26
+        
+    if Td > 2.37:
+        Td = 2.37
+        
+    return [K, Ti, Td]
+    
+    
+def crossover(lst):
+    final = []
+  
+    for i in range(0, len(lst) - 1, 2):
+        parent1 = lst[i]
+        parent2 = lst[i+1]
+        rand = random.random()
+        
+        # Do a crossover
+        if rand < 0.6:
+            # Whole arithmetic crossover used
+            alpha = 0.9
+            
+            #child 1
+            K1 = alpha*parent1['params'][0] + (1-alpha)*parent2['params'][0]
+            Ti1 = alpha*parent1['params'][1] + (1-alpha)*parent2['params'][1]
+            Td1 = alpha*parent1['params'][2] + (1-alpha)*parent2['params'][2]
+            
+            final.append({"params": check_limits(K1, Ti1, Td1),
+                          "fit": round(fitnessFunction(K1, Ti1, Td1), 3)})
+                          
+            #child 2
+            K2 = alpha*parent2['params'][0] + (1-alpha)*parent1['params'][0]
+            Ti2 = alpha*parent2['params'][1] + (1-alpha)*parent1['params'][1]
+            Td2 = alpha*parent2['params'][2] + (1-alpha)*parent1['params'][2]
+            
+            final.append({"params": check_limits(K2, Ti2, Td2),
+                          "fit": round(fitnessFunction(K2, Ti2, Td2), 3)})
+        else:
+            final.append(parent1)
+            final.append(parent2)
+            
+    
+    return final
 
+
+def mutate(lst):
+    final = []
+    
+    for i in range(len(lst)):
+        rand = random.random()
+        
+        # Mutate
+        if rand < 0.25:
+            # Uniform mutation is used
+            mut_rand = random.random()
+            K = 2 + mut_rand*(18 - 2)
+            Ti = 1.05 + mut_rand*(9.42 - 1.05)
+            Td = 0.26 + mut_rand*(2.37 - 0.26)
+            
+            final.append({"params": [K, Ti, Td],
+                          "fit": round(fitnessFunction(K, Ti, Td), 3)})
+                          
+        else:
+            final.append(lst[i])
+            
+    return final
+    
     
 def genetic_algorithm(initial_population, total_gen):
     
     current_gen = 0
     current_pop = sorted(initial_population, key= lambda k: k['fit'])
     
+    fits = []
     while current_gen < total_gen:
         sum_fit = sum([x['fit'] for x in current_pop])
         avg_prob = (sum([x['fit']/sum_fit for x in current_pop]))/len(current_pop)    
@@ -92,7 +167,10 @@ def genetic_algorithm(initial_population, total_gen):
             for i in range(actual_count):
                 parent_pop.append(sol)
                     
+        random.shuffle(parent_pop)
         cross_pop = crossover(parent_pop)
+        
+        random.shuffle(cross_pop)
         mutate_pop = sorted(mutate(cross_pop), key= lambda k: k['fit'])
         
         best = mutate_pop[-1]
@@ -101,16 +179,18 @@ def genetic_algorithm(initial_population, total_gen):
         current_pop[0] = best
         current_pop[1] = best_2
         
-        sorted(current_pop, key= lambda k: k['fit'])
+        current_pop = sorted(current_pop, key= lambda k: k['fit'])
         current_gen += 1
         
-        print(current_pop[-1]["fit"])
+        print(current_pop[-1]["fit"])        
+        fits.append(current_pop[-1]["fit"])
         
-                
+    
+    plt.plot(fits)            
     return current_pop[-1]
         
 size = 50
-total_gen = 15
+total_gen = 150
 pc = 0.6
 pm = 0.25
 
